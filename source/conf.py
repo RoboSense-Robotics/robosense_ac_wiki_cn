2162a4d684bed8841c309186ed2a2b89e004fe12
hardy.zhong <hardy.zhong@robosense.cn>
2025-04-24 15:27:02 +0800

    new backend

--- source/conf.py
+++ source/conf.py
@@ -63,7 +63,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ['custom.css']
-html_js_files = ['custom.js']
+html_js_files = [
+    'custom.js',
+    'chatbot.js'
+]
 
 master_doc = 'index'
 
